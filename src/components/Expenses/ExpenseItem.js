4b505222b59627c02249690b25941949aef47220
darchievboris <darchievboris@gmail.com>
2021-12-08 18:29:33 -0500

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import {useState} from "react";
+
+function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title)
+
+    const clickHandler = () => {
+        setTitle("update")
+        console.log(title)
+    }
+    return (
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date}/>
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+
+                    <buttom onClick={clickHandler}>change</buttom>
+
+                </div>
+            </Card>
+        </li>
+    )
+}
+
+export default ExpenseItem
---
+++ src/components/newExpense/NewExpense.js
@@ -0,0 +1,38 @@
+import ExpenseForm from './ExpenseForm'
+import './NewExpense.css'
+import {useState} from "react";
+
+const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const saveExpenseDataHandler = (enteredExpenseDate) => {
+
+        const expenseDate = {
+            ...enteredExpenseDate,
+            id: Math.random().toString()
+        }
+        props.onAddExpense(expenseDate)
+        setIsEditing(false);
+    }
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+    return (
+        <div className='new-expense'>
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
+        </div>
+    )
+}
+
+export default NewExpense
